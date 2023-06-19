c1820e6be7045c8ef7e4c34ebc24d26b6b63d103
Caio Souza <kcaiosouza@gmail.com>
2023-06-19 15:30:55 -0300

    1.0 - Design

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# redepharma-run23
+# Corrida Redepharma
+
+A corrida redepharma é um evento esportivo que ocorre todo ano providenciada pela Redepharma LTDA. Esse repositório é referente ao desenvolvimento do sistema de inscrição e acompanhamento dos participantes.
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,118 +1,157 @@
 import Image from 'next/image'
+import Marquee from 'react-fast-marquee';
 import { Inter } from 'next/font/google'
+import { HiPhone, HiMapPin } from 'react-icons/hi2'
+import { HiAtSymbol } from 'react-icons/hi'
 
 const inter = Inter({ subsets: ['latin'] })
+
+import styles from '@/styles/Index.module.css'
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`${styles.container} ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <nav className={styles.navigation}>
+        <img src='smallLogoRede.png' alt='LogoRedepharma'/>
+        <div className={styles.links}>
+          <a href='#sobre'>SOBRE</a>
+          <a href='#percursos'>PERCURSO</a>
+          <a href='#kit'>KIT ATLETA</a>
+          <a href='/'>INSCREVA-SE</a>
+        </div>
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.sectionMain}>
+          <img src='bigLogo.png' alt='LogoRun2023'/>
+          <div className={styles.btnInscrever}>INSCREVA-SE</div>
+        </div>
+        <img className={styles.runner} src='woman-running.png' alt='Corredora'/>
+      </header>
+      <div className={styles.yellowSection}>
+        <div className={styles.textArea}>
+          <h3>Redepharma RUN 2023: O ápice da superação e emoção.</h3>
+          <span>Não há nada melhor do que correr pela sua saúde, pelo seu bem-estar e pela sua diversão. E é por isso que convidamos você a participar da nossa corrida anual em Campina Grande. Nós preparamos um percurso incrível para desafiar seus limites e proporcionar momentos inesquecíveis. Venha sentir a emoção de cruzar a linha de chegada e se tornar parte da nossa história. Não perca essa oportunidade única de se divertir e se superar!</span>
+        </div>
+        <img src='paraibaMap.png'/>
+      </div>
+      <Marquee pauseOnHover={true} speed={70} gradientColor={[230, 233, 242]} className={styles.patrocinadores}>
+        <div className={styles.providers} id="sobre">
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+            <img src='sandoz.png' alt='sandoz o patrocinador'/>
+        </div>
+      </Marquee>
+      <div className={styles.aboutSection}>
+        <h2>SOBRE A CORRIDA</h2>
+        <p>Em Campina Grande, a prática de atividade física é constante. É comum observar pessoas em torno do Açude Velho, no Parque da Criança e em vários outros lugares em busca da qualidade de vida fazendo caminhadas, pedalando, andando de skate ou de patins.</p>
+        <p id="percursos">Dentre tantas modalidades, o destaque é para corrida de rua, que ganha admiradores em todo o Brasil, e em Campina Grande, não é diferente. E como o Assunto é saúde e qualidade de vida, a Redepharma traz esse grande benefício para Campina Grande – O Circuito Redepharma RUN.</p>
+      </div>
+      <div className={styles.routesSection}>
+        <div className={styles.card}>
+          <div className={styles.cardHeader}>
+            <span>Distancia 3km</span>
+            <div className={styles.cardButton}>Correr!</div>
+          </div>
+          <div className={styles.mapImage3}>
+            <span>Ver ruas</span>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeader}>
+            <span>Distancia 5km</span>
+            <div className={styles.cardButton}>Correr!</div>
+          </div>
+          <div className={styles.mapImage5}>
+            <span>Ver ruas</span>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeader}>
+            <span>Distancia 10km</span>
+            <div className={styles.cardButton}>Correr!</div>
+          </div>
+          <div className={styles.mapImage10}>
+            <span>Ver ruas</span>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeader}>
+            <span>Distancia 15km</span>
+            <div className={styles.cardButton}>Correr!</div>
+          </div>
+          <div className={styles.mapImage15}>
+            <span id="kit">Ver ruas</span>
+          </div>
+        </div>
+        
+      </div>
+      <div className={styles.kitSection}>
+        <div className={styles.invoice}>
+          <div className={styles.cardInvoice}>
+            <div className={styles.cardInvoiceHeader}>
+              <h3>Sobre o KIT</h3>
+              <div>
+                <div className={styles.smallLetters}>
+                  <span>Descrição</span>
+                  <span>Valor</span>
+                </div>
+                <div className={styles.infoInvoice}>
+                  <span>Corrida</span>
+                  <span>R$ 100,00</span>
+                </div>
+                <ul className={styles.listItems}>
+                  <li>Mochila</li>
+                  <li>Camisa</li>
+                  <li>Produtos</li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.subTotal}>
+              <span>Valor total</span>
+              <span>R$ 100,00</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.getKit}>
+          <div className={styles.cardGetKit}>
+            <h2>Retirada do KIT</h2>
+            <p>A retirada do kit acontecerá entre os dias 19 e 21 de outubro.</p>
+            <p>Local da retirada: Redepharma R50, Av. Manoel Tavares,400 - Jardim Taveres.</p>
+            <p>Levar documento com foto e o cartão de confirmação, que será gerado ao final da inscrição.</p>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.photosSection}>
+        <div className={styles.photo1}/>
+        <div className={styles.photo2}/>
+        <div className={styles.photo3}/>
+        <div className={styles.photo4}/>
+        <div className={styles.photo5}/>
+        <div className={styles.photo6}/>
+        <div className={styles.photo7}/>
+        <div className={styles.photo8}/>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.contactSection}>
+        <h2>CONTATO</h2>
+        <div><HiAtSymbol size={22}/> <span>run@redepharma.com.br</span></div>
+        <div><HiPhone size={22}/> <span>(83) 3315-6565</span></div>
+        <div><HiMapPin size={22}/> <span>Rua Marquês do Herval, 98 – Centro, Campina Grande - PB</span></div>
       </div>
+
+      <div className={styles.finalLine}/>
     </main>
   )
 }
